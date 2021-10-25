1531699d7fb0b872f3a4954ae444d28cb2260e3e
Oksana Oberemok <Pylypenkok@gmail.com>
2021-10-25 19:55:53 +0300

    made functions createTask and ShowNote

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 class Note {
-  constructor(topic, body) {
+  constructor(id, topic, body) {
+    this.id = id;
     this.topic = topic;
     this.body = body;
   }
 }
 
+let currentId = 1;
 let notes = [];
+let activeNote;
 
 document.addEventListener("DOMContentLoaded", () => {
   let topicInput = document.querySelector("#topic");
@@ -14,35 +17,50 @@ document.addEventListener("DOMContentLoaded", () => {
   let showAllBtn = document.querySelector(".show-all-btn");
   let list = document.querySelector(".list");
   let fullNote = document.querySelector(".full-note");
-  let activeNotes = document.querySelectorAll(".active");
 
   addBtn.addEventListener("click", () => {
     if (topicInput.value === "" || noteBodyInput.value === "") {
       alert("Please type something");
     } else {
-      notes.push(new Note(topicInput.value, noteBodyInput.value));
+      notes.push(new Note(currentId++, topicInput.value, noteBodyInput.value));
 
-      createTask();NOte
+      createTask();
       topicInput.value = "";
       noteBodyInput.value = "";
-      console.log(notes);
     }
   });
 
   list.addEventListener("click", (e) => {
     if (e.target.matches(".note-topic")) {
-      activeNotes.forEach((active) => {
-        active.classList.remove("active");
+      let activeNotes = document.querySelectorAll(".active");
+      activeNotes.forEach((activeNote) => {
+        activeNote.classList.remove("active");
       });
       e.target.classList.add("active");
+      activeNote = notes.find(
+        (note) => note.id === parseInt(e.target.dataset.id)
+      );
+      showNote();
     }
   });
 
+  function showNote() {
+    fullNote.innerHTML = "";
+    let title = document.createElement("h2");
+    title.innerHTML = activeNote.topic;
+    let noteBody = document.createElement("div");
+    noteBody.innerHTML = activeNote.body;
+
+    fullNote.append(title, noteBody);
+  }
+
   function createTask() {
     if (notes.length > 0) {
+      list.innerHTML = "";
       notes.forEach((note) => {
         let li = document.createElement("li");
         li.classList.add("note-topic");
+        li.dataset.id = note.id;
         li.innerHTML = note.topic;
         let arrow = document.createElement("i");
         arrow.classList.add("fas", "fa-caret-right");
@@ -50,11 +68,5 @@ document.addEventListener("DOMContentLoaded", () => {
         list.append(li);
       });
     }
-  }
-
-  function activeNote() {}
-
-  function showNote() {
-    let title = document.createElement("h2");
   }
 });
